8013926defe7149dd82fe5e8d4ec7b17dbb8af24
Aaron Imbrock <aaron@sizza.net>
2023-04-17 23:12:52 -0600

    Head Main Foot working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
-import { 'Component' } from React;
+import { Component } from 'react';
+
+class Header extends Component {
+    render() {
+      return (
+        // <h1>{this.props.title}</h1>
+        <h1>City Explorer</h1>
+      );
+    }
+  }
+
+  export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,11 @@ import React from 'react';
 import axios from 'axios';
 
 class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
     render() {
-        return(
-          <>
-            <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
-            <button onClick={this.getLocation}>Explore!</button>
-            {this.state.location.place_id && 
-              <h2>The city is: {this.state.location.display_name}</h2>
-            }
-          </>
+        return (
+            <h2>Pokemon Data</h2>
         )
-      }
     }
+}
 
-export default Main; 
+export default Main;
